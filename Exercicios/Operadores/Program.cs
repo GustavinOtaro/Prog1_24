ae6722ca64605baf75401654ceb3fc1ef7e94657
GustavinOtaro <gustavotite99@gmail.com>
2024-03-12 12:38:28 -0300

    Operação

---
+++ Exercicios/Operadores/Program.cs
@@ -0,0 +1,57 @@
+﻿bool loop = true; 
+while(loop) 
+{
+    WriteLine("Qual operador aritmetico deseja selecionar: (+ - * /) ou escreva 'finalizar' para encerrar o programa ?");
+    string? operador = ReadLine();
+
+    if(operador.Equals("finalizar"))
+        break;
+
+    switch(operador)
+    {
+        case "+":
+            for (float n2 = 1; n2 <= 9; n2++)
+            {
+                for (float n1 = 1; n1 <= 9; n1++)
+                {
+                    WriteLine($"{n1} + {n2} = {n1 + n2}");
+                }
+                WriteLine("______________________________");
+            }
+            break;
+        case "-":
+            for (float n2 = 1; n2 <= 9; n2++)
+            {
+                for (float n1 = 1; n1 <= 9; n1++)
+                {
+                    WriteLine($"{n1} - {n2} = {n1 - n2}");
+                }
+                WriteLine("______________________________");
+            }
+            break;
+        case "*":
+            for (float n2 = 1; n2 <= 9; n2++)
+            {
+                for (float n1 = 1; n1 <= 9; n1++)
+                {
+                    WriteLine($"{n1} * {n2} = {n1 * n2}");
+                }
+                WriteLine("______________________________");
+            }
+            break;
+        case "/":
+            for (float n2 = 1; n2 <= 9; n2++)
+            {
+                for (float n1 = 1; n1 <= 9; n1++)
+                {
+                    WriteLine($"{n1} /  {n2} = {n1 / n2}");
+                }
+                WriteLine("______________________________");
+            }
+            break;
+        default:
+            WriteLine("Operação Invalida!!");
+            break;
+    }
+    
+}
