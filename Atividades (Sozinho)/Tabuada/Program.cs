7e03a7b358a15883cc4aa094baf8912a95c11934
GustavinOtaro <gustavotite99@gmail.com>
2024-03-11 21:47:23 -0300

    Tabuada

---
+++ Atividades (Sozinho)/Tabuada/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+// Escreva um programa C# que seja capaz de perguntar ao usuario
+// um operador aritmetico especifico (+ - * /) e com base na operação 
+// escolhida pelo usuario imprima na tela a tabuada de 1 a 9 daquela operação
+// Ultilize uma formatação de impressão e laço de repetição while ou for para
+// exibir a tabuada completa dinamicamente.
+
+string? operador = ReadLine();
+
